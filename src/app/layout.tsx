d6aee584e740fb68f078e0fa7ef35cf76a09f7f3
jh <kdsocks5@naver.com>
2024-02-12 18:29:07 +0900

    speed, anal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="cupcake">
+      <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   );
